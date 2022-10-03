926fcd4bec485126a1727452c08779c1be0f5177
dinkmsd <85824110+dinkmsd@users.noreply.github.com>
2022-10-04 02:22:24 +0700

    add lab2 ex7

--- STM32/LAB2/lab2/Core/Src/software_timer.c
+++ STM32/LAB2/lab2/Core/Src/software_timer.c
@@ -8,10 +8,19 @@
 
 #include "software_timer.h"
 
+int timer0_flag = 0;
+int timer0_counter = 0;
+
 int timer1_flag = 0;
 int timer1_counter = 0;
+
 int timer2_flag = 0;
 int timer2_counter = 0;
+
+void setTimer0(int duration) {
+	timer0_counter = duration;
+	timer0_flag = 0;
+}
 
 void setTimer1(int duration) {
 	timer1_counter = duration;
@@ -24,6 +33,13 @@ void setTimer2(int duration) {
 }
 
 void timerRun() {
+	if (timer0_counter > 0) {
+			timer0_counter--;
+			if (timer0_counter <= 0) {
+				timer0_flag = 1;
+			}
+		}
+
 	if (timer1_counter > 0) {
 		timer1_counter--;
 		if (timer1_counter <= 0) {
